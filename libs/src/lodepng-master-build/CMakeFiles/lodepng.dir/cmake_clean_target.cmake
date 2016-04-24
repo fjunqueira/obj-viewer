09b2f5b8a0f6802d78ad38c1bf1e068a9f74161a
fjunqueira <fabio.blumm18@gmail.com>
2016-04-23 21:01:04 -0300

    removing libs

--- libs/src/lodepng-master-build/CMakeFiles/lodepng.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/fabio/workspace/obj-viewer/libs/lodepng/lib/liblodepng.a"
-)
--- libs/src/lodepng-master-stamp/verify-lodepng-master.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/fabio/workspace/obj-viewer/libs/download/master.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
